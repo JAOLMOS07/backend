9c35b4c58b626926b1c673b681a1fa4b9ef96669
JAOLMOS07 <jhogerolmos10@gmail.com>
2023-07-02 23:33:07 -0500

    Modificando test

---
+++ tests/Feature/BooksApiTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+ use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BooksApiTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     */
+    function can_get_all_books(): 
+    {
+    
+    }
+}
